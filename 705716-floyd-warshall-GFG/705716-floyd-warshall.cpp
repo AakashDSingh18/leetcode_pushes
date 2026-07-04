519bb476c70db1f25d742458beea03a4f824cb50
Aakash Deep Singh <aakash.singh3306@gmail.com>
2026-07-04 13:04:52 +0530

    Added solution - LeetHub

---
+++ 705716-floyd-warshall-GFG/705716-floyd-warshall.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    void floydWarshall(vector<vector<int>> &dist) {
+        int n= dist.size();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n;j++){
+                for(int k=0; k<n; k++){
+                    if(dist[j][i]!= 1e8 && dist[i][k]!= 1e8 && dist[j][i]+dist[i][k] < dist[j][k]){
+                        dist[j][k]= dist[j][i]+dist[i][k];
+                    }
+                }
+            }
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
