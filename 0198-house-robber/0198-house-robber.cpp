7b7479ea2d3f54408a56295a21ef042c1e462cf0
Aakash Deep Singh <aakash.singh3306@gmail.com>
2026-07-08 02:45:48 +0530

    Time: 0 ms (100%), Space: 10.2 MB (78.67%) - LeetHub - Time: O(n), Space: O(1)

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,16 +2,27 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n= nums.size();
-        vector<int> dp(n, -1);
-        dp[0]= nums[0];
-        if(n==1) return dp[0];
-        dp[1]= max(nums[0], nums[1]);
-        if(n==2) return dp[1];
-        for(int i=2; i<n; i++){
-            // if(i<=1) dp[i]= nums[i];
-            dp[i]= max(dp[i-2]+nums[i], dp[i-1]);
+        // vector<int> dp(n, -1);
+        // dp[0]= nums[0];
+        // if(n==1) return dp[0];
+        // dp[1]= max(nums[0], nums[1]);
+        // if(n==2) return dp[1];
+        // for(int i=2; i<n; i++){
+            // dp[i]= max(dp[i-2]+nums[i], dp[i-1]);
+        // }
+        // return dp[n-1];
+
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0], nums[1]);
+        int a= 0;
+        int b=0;
+        int curr= 0;
+        for(int i=0; i<n; i++){
+            curr= max(a+nums[i], b);
+            a= b;
+            b= curr;
         }
-        return dp[n-1];
+        return curr;
     }
 };
 
