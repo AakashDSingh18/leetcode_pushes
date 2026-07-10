8c67a53013ce2365c6f0cc85520ec90eca47d6c0
Aakash Deep Singh <aakash.singh3306@gmail.com>
2026-07-10 20:08:33 +0530

    Time: 35 ms (47.35%), Space: 31.4 MB (59.22%) - LeetHub - Time: O(n^2), Space: O(n^2)

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n=s.size();
+        string s2= s;
+        reverse(s.begin(), s.end());
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(s[i-1]==s2[j-1]){
+                    dp[i][j]= 1+ dp[i-1][j-1];
+                }else{
+                    dp[i][j]= max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        return n-dp[n][n];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
