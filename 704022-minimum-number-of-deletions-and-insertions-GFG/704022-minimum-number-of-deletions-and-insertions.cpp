231d6d1832445908e9f49e3688a3fd78b2104c6c
Aakash Deep Singh <aakash.singh3306@gmail.com>
2026-07-10 20:37:01 +0530

    Added solution - LeetHub

---
+++ 704022-minimum-number-of-deletions-and-insertions-GFG/704022-minimum-number-of-deletions-and-insertions.cpp
@@ -0,0 +1,23 @@
+class Solution {
+
+  public:
+    int minOperations(string &s1, string &s2) {
+        int n=s1.size();
+        int m=s2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]= 1+ dp[i-1][j-1];
+                }else{
+                    dp[i][j]= max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        return (n-dp[n][m])+(m-dp[n][m]);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
