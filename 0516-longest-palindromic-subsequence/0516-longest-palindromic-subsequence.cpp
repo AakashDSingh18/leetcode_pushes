29f9123d1a445068b642a1398ed577229076a908
Aakash Deep Singh <aakash.singh3306@gmail.com>
2026-07-10 19:59:25 +0530

    Time: 96 ms (13.98%), Space: 76 MB (44.41%) - LeetHub - Time: O(n^2), Space: O(n^2)

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -4,20 +4,19 @@ public:
         int n=s.size();
         string s2= s;
         reverse(s.begin(), s.end());
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(dp[i][j]!= -1) continue;
-                if(s[i]==s2[j]){
-                    dp[i][j]= 1+ ((i-1>=0 && j-1>=0)? dp[i-1][j-1]: 0);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(s[i-1]==s2[j-1]){
+                    dp[i][j]= 1+ dp[i-1][j-1];
                 }else{
-                    int skip1= ((j-1>=0)? dp[i][j-1]: 0);
-                    int skip2= ((i-1>=0)? dp[i-1][j]: 0);
+                    int skip1= dp[i][j-1];
+                    int skip2= dp[i-1][j];
                     dp[i][j]= max(skip1, skip2);
                 }
             }
         }
-        return dp[n-1][n-1];
+        return dp[n][n];
     }
 };
 
