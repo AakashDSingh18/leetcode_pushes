09f9d9b3b9a678c1b31c089fa0eb58926d1dbd42
Aakash Deep Singh <aakash.singh3306@gmail.com>
2026-09-06 14:15:29 +0530

    Time: 35 ms (34.2%), Space: 26.8 MB (72.62%) - LeetHub - Time: O(n*m), Space: O(n*m)

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
-    int findall(int i, int j, int& n, int& m, string s, string t, vector<vector<int>>& dp){
-        if(n-i < m-j) return 0;
-        if(j==m) return 1;
-        if(i==n) return (j==m)? 1: 0;
-        if(dp[i][j]!= -1) return dp[i][j];
-        int n_pick= findall(i+1, j, n, m, s, t, dp);
-        int pick= (s[i]==t[j])? findall(i+1, j+1, n, m, s, t, dp): 0;
-        return dp[i][j]= n_pick+pick;
-    }
+    // MEMOIZATION
+    // int findall(int i, int j, int& n, int& m, string s, string t, vector<vector<int>>& dp){
+    //     if(n-i < m-j) return 0;
+    //     if(j==m) return 1;
+    //     if(i==n) return (j==m)? 1: 0;
+    //     if(dp[i][j]!= -1) return dp[i][j];
+    //     int n_pick= findall(i+1, j, n, m, s, t, dp);
+    //     int pick= (s[i]==t[j])? findall(i+1, j+1, n, m, s, t, dp): 0;
+    //     return dp[i][j]= n_pick+pick;
+    // }
     int numDistinct(string s, string t) {
         int n= s.size();
         int m= t.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return findall(0, 0, n, m, s, t, dp);
+        // vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        // return findall(0, 0, n, m, s, t, dp);
+
+        // TABULATION
+        vector<vector<unsigned int>> dp(n + 1, vector<unsigned int>(m + 1, 0));
+        for(int i=0; i<=n; i++) dp[i][m]= 1;
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                if(s[i]==t[j]) dp[i][j]+= dp[i+1][j+1];
+                dp[i][j]+= dp[i+1][j];
+            }
+        }
+        return dp[0][0];        
     }
 };
 
